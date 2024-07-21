ae92bcf742f36e776780226629ca69c8cf88922b
Victor Tavares <victor.tavares314@outlook.com>
2024-07-21 15:09:58 -0300

    change template file extension

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_iam_policy" "test_policy" {
     description = "A test policy rendered from a directory template"
 
     policy = templatefile(
-        "${path.module}/policy_templates/role_policy.json",
+        "${path.module}/policy_templates/role_policy.tftpl",
         {
             control_lf_role_arns = jsonencode(local.role_arns)
         }
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "test_policy" {
 resource "aws_iam_role" "test_role" {
     name = "test_role_template_dir"
 
-    assume_role_policy = file("${path.module}/trust_templates/trust_policy.json")
+    assume_role_policy = file("${path.module}/trust_templates/trust_policy.tftpl")
     
     managed_policy_arns = [aws_iam_policy.test_policy.arn]
     
